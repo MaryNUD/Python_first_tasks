58ada627b3a04d2c0fad236eda21033ec6dd97c1
Mary Nudnova <m.nudnova@yandex.ru>
2024-07-29 12:12:52 +0300

    add interpolation?

--- first_try.py
+++ first_try.py
@@ -3,7 +3,15 @@ n = None
 m = 123
 print (n, m)'''
 
-print ('Enter the value of the variable r')
+'''print ('Enter the value of the variable r')
 r = input()
 # print (r)
-print (type(r))
+print (type(r))'''
+
+a = 3
+b = 11
+s = 2022
+print (a, b, s)
+# интерполяция
+print ('{} - {} - {}'.format(a,b,s))
+print (f'first - {a} second - {b} third - {s}')
