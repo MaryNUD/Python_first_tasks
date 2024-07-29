28f01a2b6fd18cb294f658c57f943bdaddd0d70f
Mary Nudnova <m.nudnova@yandex.ru>
2024-07-29 14:09:37 +0300

    add a cycle to find the smallest divider

--- first_try.py
+++ first_try.py
@@ -48,7 +48,19 @@ if username == 'Mary':
 else: 
     print ('Sorry, you cannot enter!')'''
 
-n = 85
+'''n = 85
 while n >= 5:
     n //= 5
-print (n)
+print (n)'''
+
+n = int(input())
+flag = True
+i = 2
+while flag:
+    if n % i == 0: # если остаток при делении числа n на i равен 0
+        flag = False
+        print(i)
+    elif i > n // 2: # делить числа не может превышать введенное число, деленное на 2
+        print(n)
+        flag = False
+    i += 1
