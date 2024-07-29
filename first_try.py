f20626c8d9d24790d446980103acd32512935c30
Mary Nudnova <m.nudnova@yandex.ru>
2024-07-29 12:24:33 +0300

    add change of type

--- first_try.py
+++ first_try.py
@@ -8,10 +8,17 @@ r = input()
 # print (r)
 print (type(r))'''
 
-a = 3
+'''a = 3
 b = 11
 s = 2022
 print (a, b, s)
 # интерполяция
 print ('{} - {} - {}'.format(a,b,s))
-print (f'first - {a} second - {b} third - {s}')
+print (f'first - {a} second - {b} third - {s}')'''
+
+n = 1.345
+print (n * 2)
+print (int(n))
+m = '234'
+print (m * 2)
+print (int(m) * 2)
