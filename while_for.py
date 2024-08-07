5f5a17f3a609516fd48a2b1e4861ce6bbee03ce8
Mary Nudnova <m.nudnova@yandex.ru>
2024-08-07 14:57:25 +0300

    add new file and completed the first task

---
+++ while_for.py
@@ -0,0 +1,21 @@
+# task number 1
+
+'''Задача №1. По данному целому неотрицательному n вычислите значение n!. 
+N! = 1 * 2 * 3 * … * N (произведение всех чисел от 1 до N) 0! = 1 
+Решить задачу используя цикл while Input: 5 Output: 120'''
+
+'''
+print ('Enter your number to count its factorial:')
+n = int(input())
+if n < 0:
+    print ('You entered a negative number')
+result = 1
+i = 1
+while i <= n:
+    if n == 0:
+        print (result)
+    else:
+        result = result * i
+        i += 1
+        print (result)
+'''
